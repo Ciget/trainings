5e1d9b7df76e9d9a933aef1a3abc4363de75ad38
Viktor Bilokin <victor.bilokin@gmail.com>
2022-08-21 00:37:16 -0700

    pow task

---
+++ src/Algo/Pow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Algo
+{
+    public class Pow
+    {
+        public double MyPow(double x, int n)
+        {
+            long N = n;
+
+            if (N < 0)
+            {
+                x = 1 / x;
+                N = -N;
+            }
+
+            double result = 1;
+            for (int i = 0; i<N; i++)
+            {
+                result = result * x;
+            }
+
+            return result;
+        }
+    }
+}
+
---
+++ test/Algo.UnitTest/MyPowTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+namespace Algo.UnitTest
+{
+    public class MyPowTest
+    {
+        Pow _engine = new Pow();
+
+        [Fact]
+        public void PositivePowResult()
+        {
+            _engine.MyPow(2, 3).Should().Be(8);
+        }
+
+        [Fact]
+        public void NegativePowResult()
+        {
+            _engine.MyPow(2, -2).Should().Be(0.25);
+        }
+    }
+}
+
