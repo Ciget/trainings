26befb183c2737c4f2925e0850b0acac3a477687
Viktor Bilokin <victor.bilokin@gmail.com>
2022-09-03 22:40:48 -0700

    Add valid parentheses use case

---
+++ src/Algo/Stack/ValidParenthesesString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Algo.Stack
+{
+    public class ValidParenthesesString
+    {
+        Stack<char> _stack = new Stack<char>();
+
+        public bool IsValid(string s)
+        {
+            foreach (var ch in s)
+            {
+                if (ch == '(' || ch == '{' || ch == '[')
+                {
+                    _stack.Push(ch);
+                }
+                if ((ch == ')' || ch == ']' || ch == '}') && _stack.Count == 0)
+                {
+                    return false;
+                }
+
+                if (ch == ')')
+                {
+                    if (_stack.Pop() != '(') return false;
+                }
+                else if (ch == '}')
+                {
+                    if (_stack.Pop() != '{') return false;
+                }
+                else if (ch == ']')
+                {
+                    if (_stack.Pop() != '[') return false;
+                }
+            }
+            return _stack.Count==0;
+        }
+    }
+}
+
---
+++ test/Algo.UnitTest/Stack/ValidParenthesesStringTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Algo.Stack;
+using FluentAssertions;
+
+namespace Algo.UnitTest.Stack
+{
+    public class ValidParenthesesStringTest
+    {
+        ValidParenthesesString _engine = new ValidParenthesesString();
+
+
+        [Theory]
+        [InlineData("()")]
+        [InlineData("[]{}")]
+        [InlineData("((){})")]
+        public void ShouldValidateCorrectCases(string val)
+        {
+            _engine.IsValid(val).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("())")]
+        [InlineData("]}")]
+
+        public void ShouldInValidateInCorrectCases(string val)
+        {
+            _engine.IsValid(val).Should().BeFalse();
+        }
+    }
+}
+
