21d52199b06ffcef045ff3257ea7b5bb9f89c27b
Viktor Bilokin <victor.bilokin@gmail.com>
2022-09-03 23:55:23 -0700

    solved daily temp (stack) task

---
+++ src/Algo/Stack/DailyTemperaturesTask.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Algo.Stack
+{
+    /// <summary>
+    /// Task #739
+    /// </summary>
+    public class DailyTemperaturesTask
+    {
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            int len = temperatures.Length;
+            var answers = new int[len];
+
+            Stack<Tuple<int, int>> _temp = new Stack<Tuple<int, int>>();
+
+            for (int index = 0; index < len; index++)
+            {
+                int currentTemp = temperatures[index];
+                while (_temp.Count > 0 && _temp.Peek().Item2<currentTemp)
+                {
+                    var element = _temp.Pop();
+                    if (element.Item2 < currentTemp)
+                    {
+                        answers[element.Item1] = index - element.Item1;
+                    }
+                }
+
+                _temp.Push(new Tuple<int, int>(index, temperatures[index]));
+            }
+
+            return answers;
+        }
+    }
+}
+
---
+++ test/Algo.UnitTest/Stack/DailyTemperatureTaskTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Algo.Stack;
+using FluentAssertions;
+
+namespace Algo.UnitTest.Stack
+{
+    public class DailyTemperatureTaskTest
+    {
+        DailyTemperaturesTask _engine = new DailyTemperaturesTask();
+
+        [Fact]
+        public void ShouldHandleCorrect()
+        {
+            var temperature = new int[] { 73, 74, 75, 71, 69, 72, 76, 73 };
+            _engine.DailyTemperatures(temperature).Should().BeEquivalentTo(new int[] { 1, 1, 4, 2, 1, 1, 0, 0 });
+        }
+
+        [Fact]
+        public void ShouldHandleCorrect2()
+        {
+            var temperature = new int[] { 30, 40, 50, 60 };
+            _engine.DailyTemperatures(temperature).Should().BeEquivalentTo(new int[] { 1, 1, 1, 0 });
+        }
+    }
+}
+
