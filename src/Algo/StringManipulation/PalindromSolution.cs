b593375377889fafa07f70e7e8f25be262846ec8
Viktor Bilokin <victor.bilokin@gmail.com>
2022-09-07 00:50:01 -0700

    added polindrom task

---
+++ src/Algo/StringManipulation/PalindromSolution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace Algo.StringManipulation
+{
+    public class PalindromSolution
+    {
+        public bool IsPalindrome(String s)
+        {
+            var query = s.Where(c => Char.IsLetterOrDigit(c)).Select(c => Char.ToLower(c));
+            var arr = query.ToArray(); // Copy array returned from query
+            Array.Reverse(arr); // Reverse arr to test palindrome
+                                // Using string compare against query result and reversed result
+            return new string(query.ToArray()) == new string(arr);
+        }
+    }
+}
---
+++ test/Algo.UnitTest/StringManipulation/PalindromSolutionTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FluentAssertions;
+using Algo.StringManipulation;
+
+namespace Algo.UnitTest.StringManipulation
+{
+    public class PalindromSolutionTest
+    {
+        PalindromSolution _engine = new PalindromSolution();
+
+        [Theory]
+        [InlineData("0P", false)]
+        [InlineData(" ", true)]
+        [InlineData("race a car", false)]
+        [InlineData("A man, a plan, a canal: Panama", true)]
+        [InlineData("a", true)]
+        public void ShouldTestEmpty(string input, bool result)
+        {
+            _engine.IsPalindrome(input).Should().Be(result);
+        }
+    }
+}
+
