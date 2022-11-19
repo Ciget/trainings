5c67ce2616ee52cf546cc379550bf2edc89d3b06
Viktor Bilokin <victor.bilokin@gmail.com>
2022-11-19 14:29:39 -0800

    add easy task about pivot index

--- src/Algo/ArrayManipulation/PivotIndexer.cs
+++ src/Algo/ArrayManipulation/PivotIndexer.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 namespace Algo.ArrayManipulation
 {
-	public class PivotIndex
+	public class PivotIndexer
 	{
-		public PivotIndex()
-		{
-		}
-	}
+        public int PivotIndex(int[] nums)
+        {
+            int sum = 0;
+            int leftSum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (leftSum == sum - nums[i] - leftSum) return i;
+                else {
+                    leftSum += nums[i];
+                }
+            }
+
+            return -1;
+        }
+    }
 }
 
--- test/Algo.UnitTest/ArrayManipulation/PivotIndexerTest.cs
+++ test/Algo.UnitTest/ArrayManipulation/PivotIndexerTest.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using Algo.ArrayManipulation;
+using FluentAssertions;
+
 namespace Algo.UnitTest.ArrayManipulation
 {
 	public class PivotIndexerTest
 	{
-		public PivotIndexerTest()
+		PivotIndexer _engine = new PivotIndexer();
+
+		[Theory]
+		[InlineData(new []{ 1, 7, 3, 6, 5, 6 }, 3)]
+        [InlineData(new[] { 1, 2,3}, -1)]
+        [InlineData(new[] { 2,1,-1 }, 0)]
+        public void ShouldFindPivotIndex(int[] nums, int result)
 		{
+			_engine.PivotIndex(nums).Should().Be(result);
 		}
 	}
 }
