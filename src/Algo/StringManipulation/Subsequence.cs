4ce1a1b998843acb3ed923b54940ad3ea7f57729
Viktor Bilokin <victor.bilokin@gmail.com>
2022-11-21 21:03:13 -0800

    added check subsequence

--- src/Algo/StringManipulation/Subsequence.cs
+++ src/Algo/StringManipulation/Subsequence.cs
@@ -3,9 +3,23 @@ namespace Algo.StringManipulation
 {
 	public class Subsequence
 	{
-		public Subsequence()
-		{
-		}
-	}
+        public bool IsSubsequence(string s, string t)
+        {
+            
+            int indexPos = 0;
+
+            if (s.Length == 0) return true;
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (t[i] == s[indexPos])
+                {
+                    indexPos++;
+                    if (indexPos >= s.Length) return true;
+                }
+            }
+            return false;
+        }
+    }
 }
 
--- test/Algo.UnitTest/StringManipulation/SubsequenceCheckTest.cs
+++ test/Algo.UnitTest/StringManipulation/SubsequenceCheckTest.cs
@@ -1,10 +1,21 @@
 ﻿using System;
+using Algo.StringManipulation;
+using FluentAssertions;
+
 namespace Algo.UnitTest.StringManipulation
 {
 	public class SubsequenceCheckTest
     {
-        public void ShouldCheckForSubsequence()
-        { }
+        Subsequence _engine = new Subsequence();
+
+        [Theory]
+        [InlineData("", "sample", true)]
+        [InlineData("abc", "sambplec", true)]
+        [InlineData("abssc", "sambplec", false)]
+        public void ShouldCheckForSubsequence(string s, string t, bool result)
+        {
+            _engine.IsSubsequence(s, t).Should().Be(result);
+        }
     }
 }
 
