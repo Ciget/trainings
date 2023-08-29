6b42942c2619739d9d960bd85aa934e6b8f2196b
Viktor Bilokin <victor.bilokin@gmail.com>
2023-08-28 22:40:42 -0700

    Added few samples to operate with array

---
+++ src/Algo/ArrayManipulation/MaxAmountOfOperations.cs
@@ -0,0 +1,65 @@
+﻿
+using System.Globalization;
+
+namespace Algo.ArrayManipulation
+{
+    //1679. Max Number of K-Sum Pairs
+    public class MaxAmountOfOperations
+    {
+        public int MaxOperations(int[] nums, int k)
+        {
+            if (nums.Length==0) return 0;
+            Array.Sort(nums);
+            int li = 0, ri = nums.Length-1;
+            int operations = 0;
+            int sum = 0;
+
+            while (li < ri)
+            {
+                sum = nums[li]+nums[ri];
+
+                if (sum == k)
+                {
+                    li++;
+                    ri--;
+                    operations++;
+                }
+                else if (sum > k)
+                {
+                    ri--;
+                }
+                else
+                {
+                    li++;
+                }
+            }
+
+            return operations;
+        }
+
+        public int MaxOperations2(int[] nums, int k)
+        {
+            if (nums.Length == 0) return 0;
+            int operations = 0;
+
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            foreach(int num in nums)
+            {
+                if (map.ContainsKey(k - num) && map[k - num] > 0)
+                {
+                    map[k - num]--;
+                    operations++;
+                }
+                else {
+                    if (!map.TryAdd(num, 1))
+                    {
+                        map[num]++;
+                    }
+                }
+            }
+
+            return operations;
+        }
+    }
+}
---
+++ test/Algo.UnitTest/ArrayManipulation/MaxAmountOfOperationsTest.cs
@@ -0,0 +1,25 @@
+﻿
+using Algo.ArrayManipulation;
+using FluentAssertions;
+
+namespace Algo.UnitTest.ArrayManipulation
+{
+    public class MaxAmountOfOperationsTest
+    {
+        MaxAmountOfOperations _engine = new MaxAmountOfOperations();
+
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4 }, 5, 2)]
+        public void ShouldBePositive(int[] nums, int k, int result)
+        {
+            _engine.MaxOperations(nums, k).Should().Be(result);
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4 }, 5, 2)]
+        public void ShouldBePositive2(int[] nums, int k, int result)
+        {
+            _engine.MaxOperations2(nums, k).Should().Be(result);
+        }
+    }
+}
